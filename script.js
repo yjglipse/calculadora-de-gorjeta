5a697dc097f0618c10e3490f2a2a32ca83640d89
monique <kathelin.silva@eaportal.org>
2024-03-28 00:04:48 -0300

    sexto commit do js e projeto finalizado

--- script.js
+++ script.js
@@ -54,6 +54,11 @@ function limpar() {
 
     document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
     document.querySelector(".total > strong").innerHTML = "R$ 0.00"
+
+    conta = 0
+    porcentagem = 0
+    pessoas = 0
+
 }
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
