157c2d8cd5a805da621eac88409d88a3058144c0
monique <kathelin.silva@eaportal.org>
2024-03-27 23:56:34 -0300

    quarto commit do js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let porcentagem = 0
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagemBotao)
+    botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagemBotao(evento) {
+function receberPorcentagem(evento) {
     botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
 
@@ -16,8 +16,16 @@ function receberPorcentagemBotao(evento) {
         }
     })
 
-    porcentagem = parseFloat(evento.target.value) / 100
+    if(evento.target.value !== "") {
+        porcentagem = parseFloat(evento.target.value) / 100
+    } else {
+        porcentagem = 0
+    }
+    console.log(porcentagem)
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
 
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
